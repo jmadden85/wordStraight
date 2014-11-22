5955957c46e3fd7a067f47f8294116eb1aaadc8d
jmadden85 <jmadden85@gmail.com>
2014-11-21 20:45:56 -0500

    more changes

--- main.js
+++ main.js
@@ -7,13 +7,25 @@
   var img = new Image();
   var imgData;
   var step = 0;
-  img.src = 'test.jpg';
+  var buttons = document.querySelectorAll('button');
 
-  img.addEventListener('load', function() {
-    //draw the test image
-    ctx.drawImage(img, 0, 0);
-    init();
-  });
+  for (var i = 0, b = buttons.length; i < b; i++) {
+    buttons[i].addEventListener('click', function (e) {
+      if (this.innerHTML === 'ONE') {
+        img.src = "test.jpg"
+      } else {
+        img.src = "test2.jpg"
+      }
+      document.querySelector('#buttons').setAttribute('class', 'hide');
+      ctx.drawImage(img, 0, 0);
+      init();
+    });
+  }
+
+  // var imgReady = function () {
+  //   ctx.drawImage(img, 0, 0);
+  //   init();
+  // };
 
   var init = function () {
     imgData = ctx.getImageData(0, 0, w, h);
@@ -26,6 +38,7 @@
       case 0:
         fix.trimHeight(info.borders);
         step++;
+        init();
         break;
       case 1:
         break;
@@ -41,8 +54,10 @@
       var red, green, blue;
       var rgbaCount = w * 4;
       var y = 0;
-      var x = 0;
+      var x = -1;
       var info = {};
+      this.inkedUp = [];
+      this.resetBordersAndCorners();
       for (var i = 0, n = data.length; i < n; i += 4) {
         red = data[i];
         green = data[i + 1];
@@ -52,7 +67,7 @@
         //Check if we reached the end of a row
         if (!(i % rgbaCount)) {
           y++;
-          x = 0;
+          x = -1;
         }
         //Check for ink on this pixel
         if (red < 130 && green < 130 && blue < 130) {
@@ -60,26 +75,45 @@
           this.inkedUp.push([x, y]);
         }
       }
+      if (step === 1) {
+        this.generateCorners(this.borders);
+      }
       info.ink = this.inkedUp;
       info.borders = this.borders;
-      // console.log(this.inkedUp, this.borders);
       return info;
     },
     generateBorders: function (data) {
       var borders = this.borders;
       var x = data[0];
       var y = data[1];
-      if (borders.top === null || y < borders.top) {
-        borders.top = y;
+      if (borders.top === null || y < borders.top[1]) {
+        borders.top = [x ,y];
       }
-      if (borders.right === null || x > borders.right) {
-        borders.right = x;
+      if (borders.right === null || x > borders.right[0]) {
+        borders.right = [x ,y];
       }
-      if (borders.bot === null || y > borders.bot) {
-        borders.bot = y;
+      if (borders.bot === null || y > borders.bot[1]) {
+        borders.bot = [x ,y];
       }
-      if (borders.left === null || x < borders.left) {
-        borders.left = x;
+      if (borders.left === null || x < borders.left[0]) {
+        borders.left = [x ,y];
+      }
+    },
+    generateCorners: function (data) {
+      var angleDirection = data.top[0] > data.left[0] ? 'away' : 'towards';
+      if (angleDirection === 'away') {
+        this.corners.topLeft = data.top;
+      } else {
+        this.corners.topLeft = data.left;
+      }
+      console.log(this.corners, this.borders);
+    },
+    resetBordersAndCorners: function () {
+      for (var key in this.borders) {
+        this.borders[key] = null;
+      }
+      for (var key in this.corners) {
+        this.corners[key] = null;
       }
     },
     borders: {
@@ -88,8 +122,14 @@
       bot: null,
       left: null
     },
+    corners: {
+      topLeft: null,
+      topRight: null,
+      botLeft: null,
+      botRight: null
+    },
     inkedUp: [],
-  }
+  };
 
   var fix = {
     clear: function () {
@@ -102,10 +142,14 @@
       // ctx.stroke();
       var trimmedImg = new Image();
       trimmedImg.src = canv.toDataURL();
-      canv.height = data.bot - data.top;
+      canv.height = data.bot[1] - data.top[1];
       this.clear();
-      ctx.drawImage(trimmedImg, 0, -(data.top));
+      ctx.drawImage(trimmedImg, 0, -(data.top[1]));
     },
+    correctAngle: function (data) {
+      ctx.beginPath();
+      // ctx.moveTo();
+    }
   };
 
   var findText = function (data) {
