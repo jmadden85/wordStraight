9f01a5cf5acd4c7f6b48212aece86d720ad8bf97
jmadden85 <joe@beardbro.com>
2015-05-30 00:14:48 -0400

    new js file

---
+++ matricide.js
@@ -0,0 +1,18 @@
+//Assume an example 10 x 10 canvas
+//0 in the matrix for whitespace
+//1 for inked space
+//var canvMatrix = [
+// [X, X, X, X, X, X, X, X, X, X, X],
+// [X, X, X, X, X, X, X, X, X, X, X],
+// [X, X, X, X, X, X, X, X, X, X, X],
+// [X, X, X, X, X, X, X, X, X, X, X],
+// [X, X, X, X, X, X, X, X, X, X, X],
+// [X, X, X, X, X, X, X, X, X, X, X],
+// [X, X, X, X, X, X, X, X, X, X, X],
+// [X, X, X, X, X, X, X, X, X, X, X],
+// [X, X, X, X, X, X, X, X, X, X, X],
+// [X, X, X, X, X, X, X, X, X, X, X],
+// [X, X, X, X, X, X, X, X, X, X, X]
+// ];
+
+
